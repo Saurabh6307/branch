c8f446ca74f5c0a303785ca7bee746c91f0277de
Saurabh6307 <ssaurabhmishra486@gmail.com>
2024-09-07 20:12:19 +0530

    for stash commit

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
 // ad some branches
 //add the new branch
+<<<<<<< Updated upstream
+=======
+//add some friend in git 
+//add stash
+//add commit
+let a=b;
+console.log(stash)
+>>>>>>> Stashed changes
